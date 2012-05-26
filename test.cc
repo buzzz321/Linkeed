142b752df543499fbcef121022136e45a0ab6dc4
Anders Olme <anders.olme@gmail.com>
2012-05-26 23:18:35 +0200

    added some google tests

--- test.cc
+++ test.cc
@@ -6,8 +6,16 @@ using namespace dast;
 using namespace std;
 
 TEST(IntList_size_test, empty) {
+	IntList uat;
+
+	EXPECT_EQ(1, uat.size() );
+}
+
+TEST(IntList_one_item_test, add_item) {
 	IntList uat(12);
 
-	cout<<uat.size()<<endl;
-	EXPECT_EQ(0, uat.size() );
+	EXPECT_EQ(1, uat.size() );
+    EXPECT_EQ(12, uat.getData() );
+    EXPECT_EQ(12, uat[0] );
 }
+
